6d3e0ec0ef8d2347bf38ab44c4302bd386e0bcc8
Kacper Krycki <kacper.krycki@bms.com.pl>
2020-12-23 22:45:28 +0100

    removed unecessary spaces

--- systemd-boot.sh
+++ systemd-boot.sh
@@ -10,9 +10,9 @@ editor no
 EOF
 
 cat > /boot/loader/entries/arch.conf<< EOF
-title   Arch Linux
-linux   /vmlinuz-linux
-initrd  /intel-ucode.img
-initrd  /initramfs-linux.img
+title Arch Linux
+linux /vmlinuz-linux
+initrd /intel-ucode.img
+initrd /initramfs-linux.img
 options root="PARTUUID=$(blkid -s PARTUUID -o value /dev/sda3)" rw
 EOF
