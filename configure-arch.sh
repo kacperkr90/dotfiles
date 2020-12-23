e1c60f5d50f7ef99f082a15be181949c67563b15
Kacper Krycki <kacper.krycki@bms.com.pl>
2020-12-23 20:36:15 +0100

    Install systemd-boot script

--- configure-arch.sh
+++ configure-arch.sh
@@ -2,8 +2,8 @@
 
 ln -sf /usr/share/zoneinfo/Poland /etc/localtime
 hwclock --systohc
-locale-gen
 sed -i 's/#pl_PL.UTF-8/pl_PL.UTF-8/' /etc/locale.gen
+locale-gen
 echo 'LANG=pl_PL.UTF-8' > /etc/locale.conf
 echo 'KEYMAP=pl' > /etc/vconsole.conf
 echo 'kacper@arch' > /etc/hostname
@@ -15,4 +15,5 @@ echo '127.0.1.1	kacper@arch.localdomain	kacper@arch' >> /etc/hosts
 passwd
 
 # bootloader
-bootctl install
+chmod u+x systemd-boot.sh
+bash systemd-boot.sh
---
+++ systemd-boot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+bootctl install
+
+cat > /boot/loader/loader.conf<< EOF
+default arch.conf
+timeout 4
+console-mode max
+editor no
+EOF
+
+cat > /boot/loader/entries/arch.conf<< EOF
+title   Arch Linux
+linux   /vmlinuz-linux
+initrd  /intel-ucode.img
+initrd  /initramfs-linux.img
+options root="LABEL=arch_os" rw
+EOF
