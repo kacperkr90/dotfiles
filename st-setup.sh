918397a01d7474439aa5e2ed4000dc474f7e2d18
Kacper Krycki <kacper.krycki@bms.com.pl>
2020-12-24 14:11:50 +0100

    solarized theme

--- st-setup.sh
+++ st-setup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-git clone https://git.suckless.org/st
+# git clone https://git.suckless.org/st
 cd st
+
+# solarized theme
+wget https://st.suckless.org/patches/solarized/st-no_bold_colors-0.8.1.diff
+wget https://st.suckless.org/patches/solarized/st-solarized-dark-20180411-041912a.diff
+
+git apply st-no_bold_colors-0.8.1.diff
+git apply st-solarized-dark-20180411-041912a.diff
+
+# install
 sudo make install
 cd ~
