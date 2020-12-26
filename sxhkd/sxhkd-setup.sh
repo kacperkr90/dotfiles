f7fdeb6a784252d35a0762964c15578f19e26f35
Kacper Krycki <kacper.krycki@bms.com.pl>
2020-12-26 16:38:53 +0100

    Added some sample keybinds

--- sxhkd/sxhkd-setup.sh
+++ sxhkd/sxhkd-setup.sh
@@ -4,4 +4,6 @@ CONFIG_DIR=$HOME/.config
 DOTFILES_DIR=$CONFIG_DIR/dotfiles
 
 echo "export XDG_CONFIG_HOME=\"\$HOME/.config\"" | sudo tee -a /etc/profile
-mkdir -p $XDG_CONFIG_HOME/sxhkd/sxhkdrc
+mkdir -p $XDG_CONFIG_HOME/sxhkd
+
+ln -s $DOTFILES_DIR/sxhkd/sxhkdrc $XDG_CONFIG_HOME/sxhkd/sxhkdrc
