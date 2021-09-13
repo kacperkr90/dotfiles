04b23f24189063ffbfaded17fe5c00f49e4b09ac
Kacper <kacper.krycki@bms.com.pl>
2021-09-13 15:26:28 +0200

    Removed default xorg config

--- configure-arch-for-user.sh
+++ configure-arch-for-user.sh
@@ -6,13 +6,6 @@ DOTFILES_DIR=$HOME/.config/dotfiles
 
 sudo pacman -S git xorg zsh plasma-desktop kde-applications aegisub lxsplit libreoffice-fresh thunderbird thunderbird-i18n-pl vlc mpv qbittorrent discord
 
-read -p "Press enter to continue"
-
-# xorg
-sudo 
-cp /usr/share/X11/xorg.conf.d/* /etc/X11/xorg.conf.d/
-ln -s $DOTFILES_DIR/.xinitrc $HOME/.xinitrc
-
 # zsh
 chmod u+x zsh/zsh-setup.sh
 ./zsh/zsh-setup.sh
